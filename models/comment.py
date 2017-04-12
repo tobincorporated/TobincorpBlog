d288110c11ad3db38b2233dbbb0bab255f8964ff
ztobin <ztobin@pasedfoundation.org>
2017-04-11 21:49:54 -0700

    Documentation

--- models/comment.py
+++ models/comment.py
@@ -3,6 +3,14 @@ from google.appengine.ext import db
 from user import User
 
 class Comment(db.Model):
+    """Stores information on each comment in the blog
+    Attributes:
+        user_id: ID number of the liking user
+        entry_id: ID number of the entry liked
+        comment: Text of the comment itself
+        created: Datetime the comment was created
+        last_modified: Datetime the comment was last modified
+    """
     user_id = db.IntegerProperty(required=True)
     entry_id = db.IntegerProperty(required=True)
     comment = db.TextProperty(required=True)
--- models/like.py
+++ models/like.py
@@ -3,6 +3,11 @@ from google.appengine.ext import db
 from user import User
 
 class Like(db.Model):
+    """Stores information on each 'like' in the blog
+    Attributes:
+        user_id: ID number of the liking user
+        entry_id: ID number of the entry liked
+    """
     user_id = db.IntegerProperty(required=True)
     entry_id = db.IntegerProperty(required=True)
 
